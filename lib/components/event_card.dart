1847459d4d3c11ab1f15f916d14e8f241fd7ca38
Valen98 <leo.wahlandtcs@gmail.com>
2024-05-08 17:58:34 +0200

    Added cards for every events

---
+++ lib/components/event_card.dart
@@ -0,0 +1,94 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class EventCard extends StatelessWidget {
+  final Map<String, dynamic> event;
+  const EventCard({super.key, required this.event});
+
+  @override
+  Widget build(BuildContext context) {
+    DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm");
+    Random random = Random();
+    int randNumber = random.nextInt(5);
+    List<Color> colors = [
+      const Color(0xff533AC7),
+      const Color(0xff3AC762),
+      const Color(0xffC73A80),
+      const Color(0xffC79F3A),
+      const Color(0xffC7a1B6),
+      const Color(0xff3AC7A3)
+    ];
+
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 350,
+        height: 200,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), color: colors[randNumber]),
+        child: Padding(
+            padding: const EdgeInsets.only(
+              top: 25,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color(0xff1D1D1D)),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Align text at the start
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        event['eventName'],
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        event['eventDesc'],
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 90,
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(
+                            (dateFormat.format(event['eventDate'].toDate())),
+                            style: const TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w300),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 145,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(right: 8.0),
+                          child: Text(
+                            'Location',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w300),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
