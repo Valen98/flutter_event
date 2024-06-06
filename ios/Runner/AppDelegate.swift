471e5f533928c8c5e4c619f2e8921c01e9115e99
Valen98 <leo.wahlandtcs@gmail.com>
2024-06-06 16:40:37 +0200

    Removed api key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,16 +4,17 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-     if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GMSApiKey") as? String {
-      GMSServices.provideAPIKey(apiKey)
-    } else {
-      print("Google Maps API key not found in Info.plist")
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        if let APIKEY = KeyManager().getValue(key: "GOOGLE_MAPS_API_KEY") as? String {
+           GMSServices.provideAPIKey(APIKEY)
+        }else {
+            print("NO API KEY FOUND")
+        }
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
