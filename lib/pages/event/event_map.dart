7ab1119daca5c69408ff6f494ba6b8bed8ecca1a
Valen98 <leo.wahlandtcs@gmail.com>
2024-06-07 16:03:15 +0200

    Added error handling for the google mpas of event

--- lib/pages/event/event_map.dart
+++ lib/pages/event/event_map.dart
@@ -16,7 +16,8 @@ class _EventMapState extends State<EventMap> {
   late GoogleMapController mapController;
   LatLng? _center;
   final Set<Marker> _markers = {};
-
+  bool _isLoading = true;
+  String? _errorMessage;
   @override
   void initState() {
     super.initState();
@@ -24,40 +25,53 @@ class _EventMapState extends State<EventMap> {
   }
 
   void _loadPlaceDetails() async {
-    final placeDetails =
-        await _eventMapService.fetchPlaceDetails(widget.addressID);
-    final location = placeDetails['geometry']['location'];
-    LatLng position = LatLng(location['lat'], location['lng']);
-
-    setState(() {
-      _center = position;
-      _markers.add(Marker(
-          markerId: const MarkerId('place_id'),
-          position: position,
-          infoWindow: const InfoWindow(title: 'Address of Event')));
-    });
+    try {
+      final placeDetails =
+          await _eventMapService.fetchPlaceDetails(widget.addressID);
+      final location = placeDetails['geometry']['location'];
+      LatLng position = LatLng(location['lat'], location['lng']);
+      setState(() {
+        _center = position;
+        _markers.add(
+          Marker(
+            markerId: const MarkerId('place_id'),
+            position: position,
+            infoWindow: const InfoWindow(
+              title: 'Selected Location',
+            ),
+          ),
+        );
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _errorMessage = e.toString();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     //Right now its a bug going back and forth between different pages. Bug with widget.addressID.
     return Center(
-      child: SizedBox(
-        height: 600,
-        width: 400,
-        child: _center == null
-            ? const Center(child: CircularProgressIndicator())
-            : GoogleMap(
-                onMapCreated: (GoogleMapController controller) {
-                  mapController = controller;
-                },
-                initialCameraPosition: CameraPosition(
-                  target: _center!,
-                  zoom: 14.0,
+        child: SizedBox(
+      height: 600.0, // Set the height of the box
+      width: 400.0, // Set the width of the box
+      child: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _errorMessage != null
+              ? Center(child: Text(_errorMessage!))
+              : GoogleMap(
+                  onMapCreated: (GoogleMapController controller) {
+                    mapController = controller;
+                  },
+                  initialCameraPosition: CameraPosition(
+                    target: _center!,
+                    zoom: 14.0,
+                  ),
+                  markers: _markers,
                 ),
-                markers: _markers,
-              ),
-      ),
-    );
+    ));
   }
 }
--- lib/services/event/event_map_service.dart
+++ lib/services/event/event_map_service.dart
@@ -6,12 +6,22 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class EventMapService extends ChangeNotifier {
   Future<Map<String, dynamic>> fetchPlaceDetails(String placeId) async {
     String apiKey = dotenv.env['GOOGLE_MAPS_API_KEY'] ?? '';
-    final response = await http.get(Uri.parse(
-        'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeId&key=$apiKey'));
+    try {
+      final response = await http.get(Uri.parse(
+          'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeId&key=$apiKey'));
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body)['result'];
-    } else {
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        if (data['status'] == 'OK') {
+          return data['result'];
+        } else {
+          throw Exception('Failed to load place details: ${data['status']}');
+        }
+      } else {
+        throw Exception('Failed to load place details: ${response.statusCode}');
+      }
+    } catch (e) {
+      print(e);
       throw Exception('Failed to load place details');
     }
   }
